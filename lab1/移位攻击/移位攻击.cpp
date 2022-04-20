d5213fd9e2decf9bc7e808157794a39aa5e4ad21
LGC <1242997157@qq.com>
2022-04-20 10:33:46 +0800

    密码学实验1

---
+++ lab1/移位攻击/移位攻击.cpp
@@ -0,0 +1,51 @@
+﻿
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string s;//输入字符串
+    cout << "请输入密文" << endl;
+
+    getline(cin, s);
+
+
+    for (int a = 0; a < 26; a++)
+
+
+    {
+        cout << "移位位数为：" << a << "时" << endl;
+        for (int i = 0; i < s.length(); i++)
+        {
+            if (s[i] >= 65 && s[i] <= 90)//大写字母
+            {
+
+                int t = s[i] - a;
+                if (t < 65)
+                    t = t + 26;//越减越小，要按26加
+
+                s[i] = (char)(t);
+
+                continue;
+            }
+
+            if (s[i] >= 97 && s[i] <= 122)//小写字母
+            {
+
+                int t = s[i] - a;
+                if (t < 97)
+                    t = t + 26;
+                s[i] = (char)(t);
+                continue;
+            }
+
+        }
+        cout << s << endl;
+
+
+
+    }
+}
+
